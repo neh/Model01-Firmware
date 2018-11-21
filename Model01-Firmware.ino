6d16ed6af46831bc3490f109caf6d55cb30ead69
Nathan Howell <nath@nhowell.net>
2018-11-20 19:37:28 -0800

    should be escape, qukey adds control

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -182,7 +182,7 @@ KEYMAPS(
    Key_Backtick,  Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Delete,
    Key_Backslash, Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_Pipe,      Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Enter,
-   Key_Tab, OSM(LeftShift), Key_LeftControl, OSM(LeftAlt),
+   Key_Tab, OSM(LeftShift), Key_Escape, OSM(LeftAlt),
    OSL(LOWER),
 
    // ___,           Key_6, Key_7, Key_8,     Key_9,      Key_0,         ___,
