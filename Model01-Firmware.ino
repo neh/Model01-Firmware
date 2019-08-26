683a25725dccf9023bd50141b46f1951f0323bfb
Nathan Howell <nath@nhowell.net>
2019-08-26 16:25:15 -0700

    nope, can't handle it any more

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -553,17 +553,16 @@ void setup() {
     kaleidoscope::plugin::Qukey(0, KeyAddr(3, 6), Key_LeftGui), // left palm key
     kaleidoscope::plugin::Qukey(0, KeyAddr(3, 9), Key_RightGui), // right palm key
 
-    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 3), Key_LeftControl),       // e/Control
-    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 12), Key_RightControl),       // t/Control
+    // kaleidoscope::plugin::Qukey(0, KeyAddr(2, 3), Key_LeftControl),       // e/Control
+    // kaleidoscope::plugin::Qukey(0, KeyAddr(2, 12), Key_RightControl),       // t/Control
 
-    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 2), Key_LeftAlt),       // o/Alt
-    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 13), Key_RightAlt),       // n/Alt
+    // kaleidoscope::plugin::Qukey(0, KeyAddr(2, 2), Key_LeftAlt),       // o/Alt
+    // kaleidoscope::plugin::Qukey(0, KeyAddr(2, 13), Key_RightAlt),       // n/Alt
 
-    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 4), Key_LeftShift),       // u/Shift
-    kaleidoscope::plugin::Qukey(0, KeyAddr(2, 11), Key_RightShift),       // h/Shift
+    // kaleidoscope::plugin::Qukey(0, KeyAddr(2, 4), Key_LeftShift),       // u/Shift
+    // kaleidoscope::plugin::Qukey(0, KeyAddr(2, 11), Key_RightShift),       // h/Shift
   )
-  // Qukeys.setTimeout(200);
-  // Qukeys.setReleaseDelay(15);
+  // Qukeys.setHoldTimeout(300);
   Qukeys.setOverlapThreshold(100);
 
   OneShot.time_out = 750;
